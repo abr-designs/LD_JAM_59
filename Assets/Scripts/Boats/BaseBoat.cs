db6b15e65d7f53d1f821c04f308179c2472bbba9
Alex Bedard-Reid <alexbedardreid@gmail.com>
2026-04-19 12:19:06 -0700

    WIP Improvements

--- Assets/Scripts/Boats/BaseBoat.cs
+++ Assets/Scripts/Boats/BaseBoat.cs
@@ -81,12 +81,12 @@ namespace Prototypes.Alex.Boats
             this.cargoType = cargoType;
             isDocked = false;
 
-            var mat = flagRenderers[0].material;
+            var mat = flagRenderers[0].materials[1];
             mat.mainTexture = shipType.GetTexture();
             for (var i = 1; i < flagRenderers.Length; i++)
             {
                 var flagRenderer = flagRenderers[i];
-                flagRenderer.sharedMaterial = mat;
+                flagRenderer.sharedMaterials[1] = mat;
             }
 
             m_moveTarget = s_dockManager.portEntranceTransform;
@@ -94,7 +94,7 @@ namespace Prototypes.Alex.Boats
             boatFlagHoist.ProcessCommunicationForMe = ProcessCommunicationForMe;
             boatFlagHoist.boatType = shipType;
             
-            //SFX.BELL_RING.PlaySound();
+            SFX.BELL_RING.PlaySound();
         }
 
         private void OnDestroy()
--- Assets/Scripts/Utilities/SnapSideCamera.cs
+++ Assets/Scripts/Utilities/SnapSideCamera.cs
@@ -1,4 +1,3 @@
-using System;
 using GameInput;
 using UnityEngine;
 
@@ -10,6 +9,10 @@ public class SnapSideCamera : MonoBehaviour
 
     [Header("Input")]
     public float inputThreshold = 0.2f; // how hard you push before switching sides
+    
+    [Header("Switch Resistance")]
+    public float switchPoint = 0f;       // usually center
+    public float switchDeadZone = 10f;   // how far past center before switching
 
     [Header("Smoothing")]
     public float smoothTime = 0.15f; // lower = snappier
@@ -39,27 +42,33 @@ public class SnapSideCamera : MonoBehaviour
         GameInputDelegator.OnMouseMoved -= OnMouseMoved;
     }
 
+
     private void Start()
     {
         currentYaw = transform.localEulerAngles.y;
-        targetYaw = leftYaw;
+        targetYaw = rightYaw;
     }
 
     private void Update()
     {
-        // Decide which side we're targeting
-        if (m_mouseXDelta > inputThreshold)
+        // Only allow switching if BOTH input and position agree
+        if (currentSide == Side.Right)
         {
-            currentSide = Side.Right;
+            if (m_mouseXDelta < -inputThreshold && currentYaw < (switchPoint - switchDeadZone))
+            {
+                currentSide = Side.Left;
+            }
         }
-        else if (m_mouseXDelta < -inputThreshold)
+        else // currently Left
         {
-            currentSide = Side.Left;
+            if (m_mouseXDelta > inputThreshold && currentYaw > (switchPoint + switchDeadZone))
+            {
+                currentSide = Side.Right;
+            }
         }
 
         targetYaw = (currentSide == Side.Left) ? leftYaw : rightYaw;
 
-        // Smooth spring-like movement
         currentYaw = Mathf.SmoothDampAngle(
             currentYaw,
             targetYaw,
